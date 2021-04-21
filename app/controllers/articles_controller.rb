6617e2dfda16620d0cf2e71141c5e34a45f1cd00
Arthur Borges <arthurborges.dev@gmail.com>
2021-04-21 14:09:46 -0300

    Add delete action to the controller and to view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,6 +25,13 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
+  end
+
   def update
     @article = Article.find(params[:id])
     
